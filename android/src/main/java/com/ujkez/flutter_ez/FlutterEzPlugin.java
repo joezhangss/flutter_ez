c6df988fbedf56242cb34866e89db2c03c632893
joezhangss <40815404+joezhangss@users.noreply.github.com>
2021-09-12 11:32:39 +0800

    适配flutter2.5.0
    适配flutter2.5.0

--- android/src/main/java/com/ujkez/flutter_ez/FlutterEzPlugin.java
+++ android/src/main/java/com/ujkez/flutter_ez/FlutterEzPlugin.java
@@ -1,18 +1,22 @@
 package com.ujkez.flutter_ez;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 /** FlutterEzPlugin */
-public class FlutterEzPlugin implements MethodCallHandler {
+public class FlutterEzPlugin implements MethodCallHandler,FlutterPlugin {
   /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_ez");
-    channel.setMethodCallHandler(new FlutterEzPlugin());
-  }
+  private MethodChannel channel;
+//  public static void registerWith(Registrar registrar) {
+//    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_ez");
+//    channel.setMethodCallHandler(new FlutterEzPlugin());
+//  }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
@@ -21,5 +25,16 @@ public class FlutterEzPlugin implements MethodCallHandler {
     } else {
       result.notImplemented();
     }
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_ez");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 }
